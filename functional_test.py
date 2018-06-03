e12b04d1c4f8743d2a4af7b4bb6063762f494102
Wang Vincent <yifeiluodi@163.com>
2018-06-03 23:04:25 +0800

    third commit selenium find element 201806032304

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,6 @@
 #启动一个Selenium webdriver,打开一个真正的firefox浏览器窗口
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 #测试组织成类的形式，继承自unittest.TestCase
@@ -15,6 +16,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
+        self.browser.implicitly_wait(3)
         self.browser.quit()
 
     #
@@ -25,9 +27,21 @@ class NewVisitorTest(unittest.TestCase):
     #
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
+        header_text=self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
         
-        self.assertIn('To-Do',self.browser.title)
-
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+             inputbox.get_attribute('placeholder'),
+             'Enter a to-do item'
+        )
+        
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text=='1:Buy peacock feathers for row in rows'))
         self.fail('Finish the test!')
 
 #
